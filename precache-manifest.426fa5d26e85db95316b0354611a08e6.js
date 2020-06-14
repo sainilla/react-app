32887c224de5f41634ed17d2acfd15571af89667
Sai <saisumanthnilla@gmail.com>
2020-06-14 13:50:05 +0530

    Updates

---
+++ precache-manifest.426fa5d26e85db95316b0354611a08e6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "331316fa73ba4d93cc8e187519ce27da",
+    "url": "/deploy_reactApp/index.html"
+  },
+  {
+    "revision": "284a83461a6679f40aba",
+    "url": "/deploy_reactApp/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "2e585b38c41a8183f3d5",
+    "url": "/deploy_reactApp/static/js/2.12ff3425.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/deploy_reactApp/static/js/2.12ff3425.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "284a83461a6679f40aba",
+    "url": "/deploy_reactApp/static/js/main.92008cb5.chunk.js"
+  },
+  {
+    "revision": "58bc2ddd1819656569ce",
+    "url": "/deploy_reactApp/static/js/runtime-main.cf6fac49.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/deploy_reactApp/static/media/logo.5d5d9eef.svg"
+  }
+]);
